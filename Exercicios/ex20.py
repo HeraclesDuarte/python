d89ba5544ce34afb93317e156d7f0329aad5d131
heracles.duarte <analistaheracles@gmail.com>
2021-09-30 17:01:13 -0300

    "Correção do Exe 20"

--- Exercicios/ex20.py
+++ Exercicios/ex20.py
@@ -9,4 +9,16 @@ n3 = input('Escreva o nome do aluno 3: ')
 n4 = input('Escreva o nome do aluno 4: ')
 lista = [n1, n2, n3, n4]
 escolhido = random.shuffle(lista)
-print('... A Ordem sorteada então será = {}.'.format(escolhido))
+print('... A Ordem sorteada então será = {}.'.format(lista))
+
+'''#Jeito do Professsor
+d
+import random
+n1 = str(input('Primeiro Aluno: '))
+n2 = str(input('Segundo  Aluno: '))
+n3 = str(input('Terceiro  Aluno: '))
+n4 = str(input('Quarto  Aluno: '))
+lista = [n1, n2, n3, n4]
+random.shuffle(lista)
+print('A Ordem de apresentação será')
+print(lista)'''
