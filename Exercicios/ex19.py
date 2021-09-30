5e778cccc798dd537740abf9784638be0743cf2c
heracles.duarte <analistaheracles@gmail.com>
2021-09-30 16:44:56 -0300

    "Comentario do Exe 19 no jeito do professor"

--- Exercicios/ex19.py
+++ Exercicios/ex19.py
@@ -16,6 +16,6 @@ n1 = input('Primeiro Aluno: ')
 n2 = input('Segundo Aluno: ')
 n3 = input('Terceiro Aluno: ')
 n4 = input('Quarto Aluno: ')
-lista = [n1, n2, n3, n4]
+lista = [n1, n2, n3, n4] # Aqui usamos uma lista que fica entre colchetes
 escolhido = choice(lista)
 print('O aluno escolhido foi = {}.'.format(escolhido))'''
