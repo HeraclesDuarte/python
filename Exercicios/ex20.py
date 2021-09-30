2f2bd85d52b6d7c4b6e11ebf9b998be83b8c976f
heracles.duarte <analistaheracles@gmail.com>
2021-09-30 17:21:39 -0300

    "Comentario do Professor do Exe 20"

--- Exercicios/ex20.py
+++ Exercicios/ex20.py
@@ -1,7 +1,7 @@
-'''O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos.
-Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.'''
-
-
+# '''O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos.
+# Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.'''
+#
+#
 import random
 n1 = input('Escreva o nome do aluno 1: ')
 n2 = input('Escreva o nome do aluno 2: ')
@@ -11,14 +11,14 @@ lista = [n1, n2, n3, n4]
 escolhido = random.shuffle(lista)
 print('... A Ordem sorteada então será = {}.'.format(lista))
 
-'''#Jeito do Professsor
-d
-import random
-n1 = str(input('Primeiro Aluno: '))
-n2 = str(input('Segundo  Aluno: '))
-n3 = str(input('Terceiro  Aluno: '))
-n4 = str(input('Quarto  Aluno: '))
-lista = [n1, n2, n3, n4]
-random.shuffle(lista)
-print('A Ordem de apresentação será')
-print(lista)'''
+# #Jeito do Professsor
+#
+# from random import shuffle
+# n1 = str(input('Primeiro Aluno: '))
+# n2 = str(input('Segundo  Aluno: '))
+# n3 = str(input('Terceiro  Aluno: '))
+# n4 = str(input('Quarto  Aluno: '))
+# lista = [n1, n2, n3, n4]
+# shuffle(lista)
+# print('A Ordem de apresentação será')
+# print(lista)
