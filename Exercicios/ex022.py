4892b822e732c890610045e36ad508ce2896b49e
heracles.duarte <analistaheracles@gmail.com>
2021-10-12 23:23:08 -0300

    "Exemplo de Pairwise"

--- Exercicios/ex022.py
+++ Exercicios/ex022.py
@@ -2,3 +2,4 @@ print("""Exercício Python 22: Crie um programa que leia o nome completo de uma 
 – O nome com todas as letras maiúsculas e minúsculas.
 – Quantas letras ao todo (sem considerar espaços).
 – Quantas letras tem o primeiro nome.""")
+
---
+++ pairwise/python-pairwise-master/apresenta_mereo.py
@@ -0,0 +1,28 @@
+import pairwise.all_pairs2
+
+all_pairs = pairwise.all_pairs2.all_pairs2
+
+"""
+Demo of the basic functionality - just getting pairwise/n-wise combinations
+"""
+
+# sample parameters are is taken from
+# http://www.stsc.hill.af.mil/consulting/sw_testing/improvement/cst.html
+
+
+parameters = [["<=299.00", "300.00",  ">=300.01"]
+            , ["Sem Cupom", "Inválido_Nro", "Inválido_expirado", "Inválido_15.01%, válido_15,00%, válido_14,99%"]
+            , ["Meliuz_Sim", "Meliuz_Não"]
+            , ["1ª_Compra_SIM", "1ª_Compra_NÃO"]
+            , ["1_Unid_MP", "2_Unid_MP", "3_Unid_MP"]
+            , ["Capital", "Interior"]
+            , ["Capital", "Interior"]
+            , ["Boleto", "Débito", "Crédito"]
+            , ["DT Pedido", "Dta Pedido +6d", "Dta pedido + 7d", "Dta pedido + 8d(Inválido)"]]
+
+
+pairwise = all_pairs(parameters)
+
+print("PAIRWISE:")
+for i, v in enumerate(pairwise):
+    print("%i:\t%s" % (i, str(v)))
