5562f04264261db4b2a44ea49d0ac82888c4423c
heracles.duarte <analistaheracles@gmail.com>
2021-10-18 18:07:43 -0300

    "Exemplo de Pairwise"

--- Exercicios/ex023.py
+++ Exercicios/ex023.py
@@ -7,24 +7,25 @@
 # Milhar:1
 # e que funcione para numeros menores
 # --------JEITO 1 DÁ ERRO COM MENOS UNIDADES --------------
-# num = int(input('Informe um numero: '))
-# n = str(num)
-# # print('\nAnalisando o número')
-# # print('Quantas numeros = ',len(n))
-#
-# print('Unidade = {}'.format(n[3]))
-# print('Dezena = {}'.format(n[2]))
-# print('Centena = {}'.format(n[1]))
-# print('Milhar = {}'.format(n[0]))
-# ------------------------------------------------------------
+# numero = int(input('Informe um numero: '))
+numero = str(input('Informe um numero: '))
+# n = str(numero)
+print('\nAnalisando o número...')
+# print('Quantas numeros = ',len(n))
 
-num = int(input('Informe um número: '))
-u = num // 1 % 10
-d = num // 10 % 10
-c = num // 100 % 10
-m = num // 1000 % 10
-print('Analisando o número {}'.format(num))
-print('Unidade {}'.format(u))
-print('Dezena {}'.format(d))
-print('Centena {}'.format(c))
-print('Milhar {}'.format(m))
+print('\nUnidade = {}'.format(numero[3]))
+print('Dezena = {}'.format(numero[2]))
+print('Centena = {}'.format(numero[1]))
+print('Milhar = {}'.format(numero[0]))
+
+# -----------JEITO CORRETO PROFESSOR--------------
+# num = int(input('Informe um número: '))
+# u = num // 1 % 10
+# d = num // 10 % 10
+# c = num // 100 % 10
+# m = num // 1000 % 10
+# print('Analisando o número {}'.format(num))
+# print('Unidade {}'.format(u))
+# print('Dezena {}'.format(d))
+# print('Centena {}'.format(c))
+# print('Milhar {}'.format(m))
