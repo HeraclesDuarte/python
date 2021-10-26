1ff49eb839c3576ee7866250f44ccfc8beeb1425
heracles.duarte <analistaheracles@gmail.com>
2021-10-26 17:23:21 -0300

    "Exemplo de Pairwise"

--- Exercicios/ex025.py
+++ Exercicios/ex025.py
@@ -1,2 +1,5 @@
 '''Exercício Python 25:
 Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.'''
+
+silva = str(input('Qual o seu nome completo: ')).strip()
+print ('Seu nome tem Silva: {} '.format('silva' in silva.lower()))
