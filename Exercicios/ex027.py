32bce638b9f683c77da68c376a54ae3669ebbffd
heracles.duarte <analistaheracles@gmail.com>
2021-11-03 08:57:47 -0300

    "Exercicio 26"

---
+++ Exercicios/ex027.py
@@ -0,0 +1,7 @@
+# '''Faça um programa que leia o nome completo de uma pessoa,
+# mostrando em seguida o primeiro e o último nome último.'''
+
+nome = str(input('Escreva um nome completo: ')).strip()
+nomed = nome.split()
+print('O primeiro nome é:{}'.format(nomed[0]))
+print('O Ultimo nome é:{}'.format(nomed[len(nomed)-1]))
