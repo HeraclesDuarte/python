cead29ef6a248f6d7ba7b4520b48a0ff8d886f9d
heracles.duarte <analistaheracles@gmail.com>
2021-12-08 18:11:16 -0300

    "Exercicio 26"

---
+++ Exercicios/ex033.py
@@ -0,0 +1,12 @@
+'''Faça um programa que leia três números e mostre qual é o maior e qual é o menor'''
+
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
+n3 = int(input('Terceiro valor: '))
+
+
+
+
+
+print('O menor valor foi {}'.format(menor))
+# print('O menor valor foi {}'.format(maior))
