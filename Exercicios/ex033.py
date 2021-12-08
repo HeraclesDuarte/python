3c11dac819b518a5a3d4559b767683da0a7563db
heracles.duarte <analistaheracles@gmail.com>
2021-12-08 18:49:42 -0300

    "Exercicio 26"

--- Exercicios/ex033.py
+++ Exercicios/ex033.py
@@ -1,6 +1,8 @@
 '''Faça um programa que leia três números e mostre qual é o maior e qual é o menor'''
 '''ver exec 20'''
 
+## MEU JEITO 1
+
 # n1 = int(input('Primeiro valor: '))
 # n2 = int(input('Segundo valor: '))
 # n3 = int(input('Terceiro valor: '))
@@ -9,27 +11,38 @@
 # print('O maior numero digitado foi: {} '.format(max(lista)))
 # print('O menor numero digitado foi: {} '.format(min(lista)))
 
+# # MEU JEITO 2
+#
+# primeiro = int(input('Primeiro valor: '))
+# segundo = int(input('Segundo valor : '))
+# terceiro = int(input('Terceiro valor: '))
+#
+# # Achando o maior numero Digitado
+# maior = primeiro
+#
+# if (segundo > maior):
+#     maior = segundo
+# if (terceiro > maior):
+#     maior = terceiro
+#
+# print('O Maior Valor é{}: '.format(maior))
+#
+# # Achando o menor número
+# menor = primeiro
+#
+# if (segundo < menor):
+#     menor = segundo
+# if (terceiro < menor):
+#     menor = terceiro
+#
+# print('O Menor valor é{}: '.format(menor))
 
-primeiro = int(input('Primeiro valor: '))
-segundo = int(input('Segundo valor : '))
-terceiro = int(input('Terceiro valor: '))
+#JEITO 1 PROFESSOR
 
-# Achando o maior numero Digitado
-maior = primeiro
-
-if (segundo > maior):
-    maior = segundo
-if (terceiro > maior):
-    maior = terceiro
-
-print('O Maior Valor é{}: '.format(maior))
-
-# Achando o menor número
-menor = primeiro
-
-if (segundo < menor):
-    menor = segundo
-if (terceiro < menor):
-    menor = terceiro
-
-print('O Menor valor é{}: '.format(menor))
+a = int(input('Primeiro valor: '))
+b = int(input('Segundo valor : '))
+c = int(input('Terceiro valor: '))
+if b<a and a<c:
+    menor = a
+if b<c and b<a:
+    menor = b
