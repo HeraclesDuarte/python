4a589caf1e374eb56dbb953b18e2eda636453c37
heracles.duarte <analistaheracles@gmail.com>
2021-12-08 18:44:02 -0300

    "Exercicio 26"

--- Exercicios/ex033.py
+++ Exercicios/ex033.py
@@ -1,12 +1,35 @@
 '''Faça um programa que leia três números e mostre qual é o maior e qual é o menor'''
+'''ver exec 20'''
 
-n1 = int(input('Primeiro valor: '))
-n2 = int(input('Segundo valor: '))
-n3 = int(input('Terceiro valor: '))
+# n1 = int(input('Primeiro valor: '))
+# n2 = int(input('Segundo valor: '))
+# n3 = int(input('Terceiro valor: '))
+# lista = [n1, n2, n3 ]
+# maior = lista
+# print('O maior numero digitado foi: {} '.format(max(lista)))
+# print('O menor numero digitado foi: {} '.format(min(lista)))
 
 
+primeiro = int(input('Primeiro valor: '))
+segundo = int(input('Segundo valor : '))
+terceiro = int(input('Terceiro valor: '))
 
+# Achando o maior numero Digitado
+maior = primeiro
 
+if (segundo > maior):
+    maior = segundo
+if (terceiro > maior):
+    maior = terceiro
 
-print('O menor valor foi {}'.format(menor))
-# print('O menor valor foi {}'.format(maior))
+print('O Maior Valor é{}: '.format(maior))
+
+# Achando o menor número
+menor = primeiro
+
+if (segundo < menor):
+    menor = segundo
+if (terceiro < menor):
+    menor = terceiro
+
+print('O Menor valor é{}: '.format(menor))
