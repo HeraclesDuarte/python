bb872358f7a018453cff72c843a87a92de0eb4c5
heracles.duarte <analistaheracles@gmail.com>
2021-12-22 18:19:08 -0300

    "Exercicio 26"

--- Exercicios/ex032.py
+++ Exercicios/ex032.py
@@ -1,6 +1,8 @@
 '''Faça um programa que leia um ano qualquer e mostre se ele é bissexto.'''
 
-#
+
+# JEITO 1
+# #
 # ano = int(input('Digite o ano: '))
 # if (ano % 4) == 0:
 #     print('Ano {} é bissexto'.format(ano))
@@ -10,11 +12,11 @@
 
 # JEITO DO PROFESSOR
 
-# from datetime import date
-# ano = int(input('Que ano quer analisar? ou Coloque 0 para analisar o ano atual:'))
-# if ano == 0:
-#     ano = date.today().year
-# if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0: #DIVISIVEL POR 4 E TAMBÉM NÃO PODE SER DIV POR 100 E NÃO PODE SER DIFERENTE DE 0 OU O ANO SER DIVISIVEL POR 400
-#     print('{} é bissexto'.format(ano))
-# else:
-#     print('{} não é bissexto'.format(ano))
+from datetime import date
+ano = int(input('Que ano quer analisar? ou Coloque 0 para analisar o ano atual:'))
+if ano == 0:
+    ano = date.today().year
+if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0): #DIVISIVEL POR 4 E TAMBÉM NÃO PODE SER DIV POR 100 E NÃO PODE SER DIFERENTE DE 0 OU O ANO SER DIVISIVEL POR 400
+    print('{} é bissexto'.format(ano))
+else:
+    print('{} não é bissexto'.format(ano))
